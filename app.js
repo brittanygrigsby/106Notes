8c1dd0da0e3b903788c918422cf98e2e35645a64
brittanygrigsby <brittany.grigsby@sdgku.edu>
2025-07-25 06:43:43 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,101 @@
-function hello()
+function saveTask()
 {
-    console.log("hello there!");
+    //get the values
+    const title = $("#txtTitle").val();
+    const desc = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#selDate").val();
+    const status = $("#selStatus").val();
+    const budget = $("#numBudget").val();
+    console.log(title, desc, color, date, status, budget);
+    //build an object
+    let data = new Task (title, desc, color, date, status, budget);
+    console.log(data);
+    
+    //display the info
+    displayTask(data);
+
+    //save the server
+    console.log("hello im the saveButton");
+    $.ajax({
+        type:"post",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        success: function(response)
+        {console.log(response);},
+        error: function(error)
+        {console.log(error);}
+    })
 }
 
+function displayTask(task){
+    let render =`<div class = "task" style="border-color:${task.color}">
+    <div class="info">
+    <h4> ${task.title}</h4>
+    <p> ${task.desc}<p>
+    </div>
 
+    <label class="status">${task.status} </label>
+    <div class="date-budget">
+    <label> ${task.date}</label>
+    <label> ${task.budget}  </label>
+    </div>
+    </div>`
+    ;
+    
+    //use the content of the object to render the list section
+    $(".list").append(render);
+}
 
+function loadTask(){
+    //get content of http:fsdiapi.azurewebsites.net/api/tasks
+    //console.log response from server
+$.ajax(
+{
+    type:"get",
+    url:"http://fsdiapi.azurewebsites.net/api/tasks",
+    success: function (response){
+        let dataJSON = JSON.parse(response);
+// render on the list section, only those messages created by you
+        for(let i=0;i<dataJSON.length;i++)
+        {
+            let currentValue = dataJSON[i]
+            if(currentValue.name == "Brittany59")
+            {
+                displayTask(currentValue);
+            }
+        }
+        console.log(response);
+        console.log(dataJSON);
+    },
+    error: function (error){
+        console.log(error);
+    }
+    })
+}
+
+function testRequest()
+{
+    $.ajax({
+        type:"GET",
+        url: "http://fsdiapi.azurewebsites.net",
+        success: function(response)
+        {
+            console.log(response);
+        },
+        error: function(error)
+        {
+            console.log(error);
+        }
+    });
+}
 
 function init()
 {
     console.log("hello im the init");
-    hello();
+    $("#btnSave").click(saveTask);
+    loadTask();
 }
 
 
